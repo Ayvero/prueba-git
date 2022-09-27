f2d4a57da39a28b821e3f1c61d3e7d9ac817a96f
Analia Burgos <analiaveronicadra@gmail.com>
2022-09-27 20:10:43 -0300

    cambios en rama

--- home.php
+++ home.php
@@ -4,6 +4,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
     <title>Document</title>
 </head>
 <body>
@@ -11,5 +12,7 @@
 
     <p> Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorem ipsum placeat consequatur cumque quam? Optio autem laboriosam in vero illo eligendi, cumque, dignissimos maiores facere, cum doloribus. Enim, aliquid. Nihil.</p>
 
+    <button type="button" class="btn btn-danger">Boton</button>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
 </body>
 </html>
