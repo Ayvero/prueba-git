957e6b3b419058ad54149e08af4a41f5afbe3d38
Analia Burgos <analiaveronicadra@gmail.com>
2022-09-27 19:47:44 -0300

    agrego parrafo nuevo

--- home.php
+++ home.php
@@ -9,5 +9,7 @@
 <body>
     <h1>Probando el funcionamiento de git</h1>
 
+    <p> Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorem ipsum placeat consequatur cumque quam? Optio autem laboriosam in vero illo eligendi, cumque, dignissimos maiores facere, cum doloribus. Enim, aliquid. Nihil.</p>
+
 </body>
 </html>
